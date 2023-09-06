1e1b891afeae142f37f08080cc79a6d0cf51f738
Alikber Rashidov <104397076+alikberg123@users.noreply.github.com>
2023-09-06 21:05:57 +0300

    update

--- install.sh
+++ install.sh
@@ -22,6 +22,8 @@ pkg install vim -y
 echo "устоновка neovim"
 pkg install neovim -y
 mv nvim /data/data/com.termux/files/home/.config/
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "устоновка python"
 # python 
 pkg install python -y
